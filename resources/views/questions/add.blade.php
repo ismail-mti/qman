70c20f7168fd6cc1014887e7c3a0f8098c4927c2
Ismail-PC\Ismail <m.ismail.naeem@gmail.com>
2018-03-26 17:30:39 +0500

    Choice Add, Delete

--- resources/views/questions/add.blade.php
+++ resources/views/questions/add.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 @section('add_question')
+<style>
+
+
+</style>
 <script>
 
     var qs_number = 0;
     var qs_index = 0;
+    var sc_number = 1;
+    var signle_choice_index = 1;
     function addNewQuesion() {
         var question =
                 "<div class='form-group' id='question_" + qs_number + "'  >" +
@@ -24,10 +30,11 @@
                 "<button  type='button' class='btn btn-default' onclick='delQuesion(" + qs_number + ")' >Delete Question</button" +
                 "</div>" +
                 "</div>" +
+                "<div class='container'><div class='row'><div class='col-md-12'>" +
                 "<div class='form-group' id='answer_section_" + qs_number + "'>" +
                 "<div class='col-md-12'>" +
                 "</div>" +
-                "</div>";
+                "</div></div></div></div>";
         qs_index = qs_number;
         qs_number++;
         $("#question_section").append(question);
@@ -37,38 +44,64 @@
         $("#question_" + qs_id).remove();
         qs_number--;
     }
-    function delChoice()
+
+    function addChoiceSingle()
     {
-        
-        
+        var choice = "<div class='col-md-4' id='qs_choices_single_" + qs_index + '_' + signle_choice_index + "'><label class='col-md-2 control-label' >Choice" + signle_choice_index + "</label></div>" +
+                "<div class='col-md-6'><label for='qs_choice_single_" + qs_index + '_' + signle_choice_index + "'>" +
+                "                       <input type='text' name='qs_choice_single_val[]' placeholder='Enter Choice' />" +
+                "                       <input type='radio' name='qs_choices[]' id='qs_choice_single_" + qs_index + '_' + signle_choice_index + "' value='1'>Is Correct?" +
+                "                   </label>" +
+                "</div>" +
+                "<div class='col-md-2'><button type='button' class='btn btn-default'>Delete Choice</button></div>" +
+                "</div>"
+        $("#qs_choices_single_sction").append(choice);
+        signle_choice_index++;
     }
+    function delChoiceSingle()
+    {
+        alert('delChoiceSingle');
+//        var id = "qs_choices_single_" + qs_index + '_' + signle_choice_index;
+//        alert(id);
+//        $("#" + id).remove();
+//        signle_choice_index--;
+    }
+    function addChoiceMul()
+    {
+
+
+    }
+    function delChoiceMul()
+    {
+
+
+    }
+
     $(document).ready(function () {
 
 //"#qs_type_" + qs_index
         $("body").on('change', 'select[id^="qs_type"]', function () {
             var qs_type_sel = $("#qs_type_" + qs_index + " option:selected").val();
-            var signle_choice_index = 1;
+
             var text_ans =
-                    "<div class='form-group'>" +
                     "<label class='col-md-4 control-label' for='answer_text'>Answer</label>" +
                     "<div class='col-md-8'>" +
                     "<input id='answer_text' name='answer_text' type='text'  class='form-control input-lg' required=''>" +
-                    "</div>" +
                     "</div>";
             var mcq_single =
-                    "<div class='form-group' id='qs_choices_single_" + qs_index + '_' + signle_choice_index + "'>" +
-                    "<label class='col-md-4 control-label'>Choice" + signle_choice_index + "</label>" +
-                    "<div class='col-md-4'>" +
-                    "<div class='radio'>" +
-                    "<label for='qs_choice_single_" + qs_index + '_' + signle_choice_index + "'>" +
-                    "<input type='text' name='qs_choice_single_val[]' placeholder='Enter Choice' />" +
-                    "<input type='radio' name='qs_choices[]' id='qs_choice_single_" + qs_index + '_' + signle_choice_index + "' value='1'>Is Correct" +
-                    "</label>" +
+                    "<div class='container'>" +
+                    "<div class='row' id='qs_choices_single_sction'>" +
+                    "<div class='col-md-4' id='qs_choices_single_" + qs_index + '_' + signle_choice_index + "'><label class='col-md-2 control-label' >Choice" + signle_choice_index + "</label></div>" +
+                    "<div class='col-md-6'><label for='qs_choice_single_" + qs_index + '_' + signle_choice_index + "'>" +
+                    "                       <input type='text' name='qs_choice_single_val[]' placeholder='Enter Choice' />" +
+                    "                       <input type='radio' name='qs_choices[]' id='qs_choice_single_" + qs_index + '_' + signle_choice_index + "' value='1'>Is Correct?" +
+                    "                   </label>" +
                     "</div>" +
-                    "<button type='button' class='btn btn-default'>Delete Choice</button>" +
+                    "<div class='col-md-2'><button type='button' class='btn btn-default' onClick='delChoiceSingle()'>Delete Choice</button></div>" +
+                    "</div>" +
+                    "<div class='col-md-2'><button type='button' class='btn btn-default' onClick='addChoiceSingle()'>Add Choice</button></div>" +
                     "</div>" +
                     "</div>";
-
             var mcq_mul =
                     "<div class='form-group'>" +
                     "<label class='col-md-4 control-label' for='qs_choice_multiple'></label>" +
@@ -84,13 +117,11 @@
             {
                 $("#answer_section_" + qs_index).empty();
                 $("#answer_section_" + qs_index).append(text_ans);
-            }
-            else if (qs_type_sel == 2)
+            } else if (qs_type_sel == 2)
             {
                 $("#answer_section_" + qs_index).empty();
                 $("#answer_section_" + qs_index).append(mcq_single);
-            }
-            else if (qs_type_sel == 3)
+            } else if (qs_type_sel == 3)
             {
                 $("#answer_section_" + qs_index).empty();
                 $("#answer_section_" + qs_index).append(mcq_mul);
